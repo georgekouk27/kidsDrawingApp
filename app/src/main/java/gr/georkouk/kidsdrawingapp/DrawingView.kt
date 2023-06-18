9f7d80261a4ed3bb82cf0dd82c3256cf666670a0
George Koukouris <georgekouk27@gmail.com>
2023-06-18 13:26:40 +0300

    Initial commit

---
+++ app/src/main/java/gr/georkouk/kidsdrawingapp/DrawingView.kt
@@ -0,0 +1,42 @@
+package gr.georkouk.kidsdrawingapp
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+
+
+class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
+
+    private var drawPath: CustomPath? = null
+    private var canvasBitmap: Bitmap? = null
+    private var drawPaint: Paint? = null
+    private var canvasPaint: Paint? = null
+    private var brushSize: Float = 0.toFloat()
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null
+
+    init{
+        setupDrawing()
+    }
+
+    private fun setupDrawing(){
+        drawPaint = Paint()
+        drawPath = CustomPath(color, brushSize)
+        drawPaint!!.color = color
+        drawPaint!!.style = Paint.Style.STROKE
+        drawPaint!!.strokeJoin = Paint.Join.ROUND
+        drawPaint!!.strokeCap = Paint.Cap.ROUND
+        canvasPaint = Paint(Paint.DITHER_FLAG)
+        brushSize = 20.toFloat()
+    }
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float): Path() {
+
+    }
+
+}
